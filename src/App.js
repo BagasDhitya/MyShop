efdaafebcfda7910c1eeca42980eeeee76ec52fa
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2022-07-16 20:46:43 +0700

    add bootstrap, font awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Home from "./components/Home";
 import Products from "./components/Products";
 import { Route, Routes } from "react-router-dom";
